7dcd9d1319ff3faa5a8a468befb09e47fdfdbaba
wei397705708 <wei397705708@126.com>
2021-07-24 22:35:04 +0800

    修改了tabs图标

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import app from "./app";
-import Mint from 'mint-ui';
-import 'mint-ui/lib/style.css'
+import Mint from "mint-ui";
+import "mint-ui/lib/style.css";
 import "./lib/css/mui.min.css";
+import "./lib/css/icons-extra.css"
 
 Vue.use(Mint);
 
